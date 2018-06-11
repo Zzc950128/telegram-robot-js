269cbccde69e50463443fe99ff9f1b15eb0bc9da
Zhicong Zhang <zhangzhicong@wildroi.com>
2018-06-11 15:55:36 +0800

    change test

---
+++ test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '559899786:AAF0icPrHjiA0WZ3-GR4SmnAD-htwYqkHZo';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+	const chatId = msg.chat.id;
+	const resp = match[1];
+	bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+	const chatId = msg.chat.id;
+	bot.sendMessage(chatId, 'Received your message');
+});
