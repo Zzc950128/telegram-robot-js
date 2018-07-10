cc10152502936e5fad593e0b265dc3064c30300f
ZhangZhicong <a2512878126@gmail.com>
2018-07-10 17:56:27 +0800

    fix

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
 const RegexpCommand = Telegram.RegexpCommand
 const TextCommand = Telegram.TextCommand
-const tg = new Telegram.Telegram('559899786:AAF0icPrHjiA0WZ3-GR4SmnAD-htwYqkHZo')
-
+//const tg = new Telegram.Telegram('559899786:AAF0icPrHjiA0WZ3-GR4SmnAD-htwYqkHZo')
+//617336269:AAGAAmLZFsJ1fgSS9FvgKtXDbXvb8sYiLWs
+const tg = new Telegram.Telegram('617336269:AAGAAmLZFsJ1fgSS9FvgKtXDbXvb8sYiLWs')
 class PingController extends TelegramBaseController {
 	pingHandler($) {
 		api.getMZ(function(data) {
--- test.js
+++ test.js
@@ -7,18 +7,25 @@
 
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '559899786:AAF0icPrHjiA0WZ3-GR4SmnAD-htwYqkHZo';
+//const token = '559899786:AAF0icPrHjiA0WZ3-GR4SmnAD-htwYqkHZo';
+const token = '617336269:AAGAAmLZFsJ1fgSS9FvgKtXDbXvb8sYiLWs';
 
 const bot = new TelegramBot(token, {polling: true});
 
+/*
 bot.onText(/\/echo (.+)/, (msg, match) => {
 	const chatId = msg.chat.id;
 	const resp = match[1];
 	bot.sendMessage(chatId, resp);
 });
+*/
 
 bot.on('message', (msg) => {
-	console.log(msg)
+	const new_chat_member = !!msg.new_chat_member || !!msg.new_chat_members;
 	const chatId = msg.chat.id;
-	// bot.sendMessage(chatId, 'Received your message');
+	const messageId = msg.message_id;
+	if(new_chat_member) {
+		bot.deleteMessage(chatId, messageId)
+	}
+	//bot.sendMessage(chatId, 'Received your message');
 });
